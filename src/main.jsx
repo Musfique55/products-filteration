275346f23be645be78b15d4da5a9f54bb033e6f2
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-08-15 12:07:50 +0600

    initial setup and regissstration page done

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Registration from './components/Registration/Registration.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Registration></Registration>
+  }
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}>
+    <App />
+    </RouterProvider>
+  </StrictMode>,
+)
